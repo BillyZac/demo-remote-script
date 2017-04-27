edcaee776678401ab8b776cb50a9776a323dafaa
Zachary Smith <william.smith1@wipro.com>
2017-04-27 16:35:31 -0400

    Fix dotfiles

--- script.sh
+++ script.sh
@@ -3,7 +3,8 @@ echo 'Hi. About to do some stuff.'
 sudo apt-get update
 sudo apt-get install -y git
 
+rm -rf $HOME/dotfiles
 git clone https://github.com/BillyZac/dotfiles.git $HOME/dotfiles
-
+cd $HOME/dotfiles
 source $HOME/dotfiles/symlink_dotfiles
 source $HOME/dotfiles/install_node_global_packages
