07eb58fa66f54d39488e4dc4306bce7531e769cf
Zachary Smith <william.smith1@wipro.com>
2017-04-27 17:06:21 -0400

    Add node

--- script.sh
+++ script.sh
@@ -3,8 +3,13 @@ echo 'Hi. About to do some stuff.'
 sudo apt-get update
 sudo apt-get install -y git
 
+# Install node
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+# Add my precious dotfiles and a few faves
 rm -rf $HOME/dotfiles
 git clone https://github.com/BillyZac/dotfiles.git $HOME/dotfiles
 cd $HOME/dotfiles
-# source $HOME/dotfiles/symlink_dotfiles
+source $HOME/dotfiles/symlink_dotfiles
 source $HOME/dotfiles/install_node_global_packages
