e4ed347de3a34a686cf2d86c2bc4f7b34e7cc67f
Zachary Smith <william.smith1@wipro.com>
2017-04-27 16:22:29 -0400

    Remove nginx. Add dotfiles.

--- script.sh
+++ script.sh
@@ -1,4 +1,8 @@
 echo 'Hi. About to do some stuff.'
 
 sudo apt-get update
-sudo apt-get install -y nginx
+sudo apt-get install -y git
+
+git clone https://github.com/BillyZac/dotfiles.git
+
+source dotfiles/install.sh
