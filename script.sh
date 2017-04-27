ce6c5ecc71db5d0f2b010c2fd61b84fc632f1965
Zachary Smith <william.smith1@wipro.com>
2017-04-27 16:47:29 -0400

    Still debugging...

--- script.sh
+++ script.sh
@@ -6,5 +6,5 @@ sudo apt-get install -y git
 rm -rf $HOME/dotfiles
 git clone https://github.com/BillyZac/dotfiles.git $HOME/dotfiles
 cd $HOME/dotfiles
-source $HOME/dotfiles/symlink_dotfiles
+# source $HOME/dotfiles/symlink_dotfiles
 source $HOME/dotfiles/install_node_global_packages
